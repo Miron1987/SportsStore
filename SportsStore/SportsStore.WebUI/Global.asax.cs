f756ef878671d4f525bb242e068c5d1065f8681d
Aleksandr Kougia <zero0072002@mail.ru>
2017-12-10 12:59:23 +0300

    no message

--- SportsStore/SportsStore.WebUI/Global.asax.cs
+++ SportsStore/SportsStore.WebUI/Global.asax.cs
@@ -5,17 +5,23 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using SportsStore.WebUI.Infrastructure;
 
 namespace SportsStore.WebUI
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
         }
     }
 }
